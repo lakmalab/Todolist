983b1220078bb5cea8efce67f14ae11623f881a0
lakma <lakmalaberathne@gmail.com>
2025-05-18 10:13:05 +0530

    temperory solution to show to do items using Tostring

--- src/main/java/controller/TodoForm.java
+++ src/main/java/controller/TodoForm.java
@@ -1,14 +1,18 @@
 package controller;
 
 import db.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import model.TodoItem;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static java.lang.Double.parseDouble;
@@ -21,7 +25,7 @@ public class TodoForm {
     public TextField txtTitle;
     @FXML
     private TableColumn<?, ?> colSalary;
-
+    ObservableList<TodoItem> todoItems = FXCollections.observableArrayList();
     @FXML
     private TableView<?> tblDone;
 
@@ -40,7 +44,7 @@ public class TodoForm {
             alert.setHeaderText("Failed to add the Item.....");
             alert.showAndWait();
         }
-
+        loadItems();
     }
 
     private boolean addTodoItem() throws SQLException {
@@ -56,6 +60,20 @@ public class TodoForm {
         int res = stm.executeUpdate();
         return res >0;
     }
+
+    private void loadItems(){
+        try {
+            ResultSet resultSet = DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT * From active_tasks");
+            while(resultSet.next()){
+                todoItems.add(new TodoItem(resultSet.getString("task_title"), resultSet.getString("task_description"), false));
+            }
+            todoListView.setItems(todoItems);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
     @FXML
     void onrowselected(MouseEvent event) {
--- src/main/java/model/TodoItem.java
+++ src/main/java/model/TodoItem.java
@@ -3,10 +3,12 @@ package model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@ToString
 public class TodoItem {
     private String title;
     private String description;
